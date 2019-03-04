0e6edfa2e4f0be3b7fa2b78a07ab3448efce88fc
huangzhibing <745216570@qq.com>
2019-03-04 16:09:38 +0800

    黄志兵

--- src/AdpterPattern/Demo1/Adapter.java
+++ src/AdpterPattern/Demo1/Adapter.java
@@ -6,9 +6,14 @@ import java.io.InputStream;
  * 适配器模式
  * 假如现在有两个接口：ReaderXml/XmlReader,现在需求中需要的用是ReaderXml的接口，恰好，XmlReader中有这个接口的功能，则可以用设配器模式。
  */
-public class Adapter extends B implements ReaderXml{
-    public InputStream readerXml() {
-        return xmlReader();
+public class Adapter implements ReaderXml{
+
+//    public InputStream readerXml() {
+//        return xmlReader();
+//    }
+    public XmlReader xmlReader;
+    public InputStream readerXml(){
+        return xmlReader.xmlReader();
     }
 }
 
